a8c9cf6efc02962cd46c45b363279c1a18abc4d2
John <jgz6@uakron.edu>
2023-02-11 10:03:40 -0500

    trusted origin added

--- myfitnesspal/settings.py
+++ myfitnesspal/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS=['web-production-8953.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['web-production-8953.up.railway.app','https://web-production-8953.up.railway.app']
 
 AUTH_USER_MODEL = "tracking.User" 
 
