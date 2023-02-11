24ae4d92638c63ac8e487b23883b75659eeeee45
John <jgz6@uakron.edu>
2023-02-11 09:32:08 -0500

    keys

---
+++ keys.py
@@ -0,0 +1,13 @@
+api_key = 'Bzx49EndyfEW4JZBL9ZJXE0FN2b9A5EVb2teWWOO'
+garmin_email = 'zoltron19@gmail.com'
+garmin_pass = 'z8z6kgEmpa6B7C'
+
+
+superuser = 'john1'
+password = 'murphylove'
+
+
+STRIPE_PUBLIC_KEY = "pk_test_51MWK1RA0pn7vugH4pmWsjGLFDrPvxQgfGzMZGlxHmU3Ufp3minL1gh0nkXKzpwMlsuO5KVPn7ULHRPHCW4k7aSC100Kjj8V0jO"
+STRIPE_SECRET_KEY = 'sk_test_51MWK1RA0pn7vugH4RQxEEAWzpqdCpa0P9gQ58qhi796ZIyuNjVTT1YBvjaeadq76thmRbeB4DXKxl195kleaJEkK00tOASUw7Q'
+STRIPE_WEBHOOK_SECRET = 'whsec_73ec963dc5c567569191b5479d9ac971896df11a5d555fa10b9c4d704153354e'
+PRODUCT_KEY = 'price_1MWKrBA0pn7vugH4RQPyV8jE'
